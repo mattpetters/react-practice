e59c9ddf9ba75699cd1ed53e7bb3c56329d71b73
Matt Petters <matt@petters.io>
2017-02-27 09:42:21 -0500

    Making changes got routing working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello!");
+import 'babel-polyfill'; //consider pulling in specific polyfills later
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router'; //history HTML5 push state
+import routes from './routes';
+import './styles/styles.css'; //webpack import css
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+    <Router history={browserHistory} routes={routes} />,
+    document.getElementById('app')
+);
