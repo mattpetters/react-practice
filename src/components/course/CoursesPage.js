c7c57b707926dd98b0936ce20a37b1f0b4a04075
Matt Petters <matt@petters.io>
2017-05-03 16:40:52 -0400

    Working on ManageCourseForm

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -6,6 +6,7 @@ import {
 } from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
     constructor(props, context) {
@@ -17,9 +18,10 @@ class CoursesPage extends React.Component {
     }
 
     render() {
+        const {courses} = this.props;
         return (<div>
             <h1> Courses </h1> 
-            {this.props.courses.map(this.courseRow)}
+            <CourseList courses={courses}/>
             </div>);
     }
 }
